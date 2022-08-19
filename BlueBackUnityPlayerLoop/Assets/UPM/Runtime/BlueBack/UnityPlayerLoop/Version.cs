0d90504f47f69a977cbab988db9e9ed86bb0452e
blueback <blueback28@gmail.com>
2022-08-19 22:06:19 +0900

    整理

--- BlueBackUnityPlayerLoop/Assets/UPM/Runtime/BlueBack/UnityPlayerLoop/Version.cs
+++ BlueBackUnityPlayerLoop/Assets/UPM/Runtime/BlueBack/UnityPlayerLoop/Version.cs
@@ -21,7 +21,7 @@ namespace BlueBack.UnityPlayerLoop
 
 		/** packageversion
 		*/
-		public const string packageversion = "0.0.19";
+		public const string packageversion = "0.0.20";
 
 		/** GetPackageVersion
 		*/
